b41e14de912a554c4fda302fc8fd574a09d77ca7
Haruki Jay Shimada <peaske16180@gmail.com>
2019-05-10 10:30:03 -0700

    adding common components from auth project

---
+++ tech_stack/src/components/common/Header.js
@@ -0,0 +1,39 @@
+// import libraries for making a component
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// make a component
+const Header = (props) => {
+   const {textStyle, viewStyle} = styles;
+
+   return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 75,
+    paddingTop: 30,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 25
+  }
+};
+
+// make a component available to other parts of the app
+// export { Header: Header }; <- normally, you would want to write something like this
+// becuase we need to export an object when using 'export * from './file...''
+// However, we can shorten the code using ES6
+
+export { Header };
