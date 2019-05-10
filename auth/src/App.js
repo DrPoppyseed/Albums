ad6de102035cede5167a78b4cc686fd1bb0c5a5f
Haruki Jay Shimada <peaske16180@gmail.com>
2019-05-09 21:49:19 -0700

    final commit to auth poject

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,28 +1,71 @@
 import firebase from 'firebase';
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, Card, CardSection, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
-    apiKey: "AIzaSyBMzks41qcwcPX_qbw2QQfPbhLp-PEo2cs",
-    authDomain: "auth-a8b8a.firebaseapp.com",
-    databaseURL: "https://auth-a8b8a.firebaseio.com",
-    projectId: "auth-a8b8a",
-    storageBucket: "auth-a8b8a.appspot.com",
-    messagingSenderId: "393931736183",
-    appId: "1:393931736183:web:ae0438adbb3aa95b"
-  });
+      apiKey: "AIzaSyBMzks41qcwcPX_qbw2QQfPbhLp-PEo2cs",
+      authDomain: "auth-a8b8a.firebaseapp.com",
+      databaseURL: "https://auth-a8b8a.firebaseio.com",
+      projectId: "auth-a8b8a",
+      storageBucket: "auth-a8b8a.appspot.com",
+      messagingSenderId: "393931736183",
+      appId: "1:393931736183:web:ae0438adbb3aa95b"
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Card>
+            <CardSection>
+              <Button onPress={() => firebase.auth().signOut()}>
+                Log out
+              </Button>
+            </CardSection>
+          </Card>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <View style={styles.spinnerStyle}>
+            <Spinner size='large' />
+          </View>
+        );
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>Auth app</Text>
+        {this.renderContent()}
       </View>
     );
+  }
+}
+
+const styles = {
+  spinnerStyle: {
+    flex: 1,
+    marginTop: 300,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 }
 
--- auth/src/components/common/index.js
+++ auth/src/components/common/index.js
@@ -2,3 +2,5 @@ export * from './Button';
 export * from './Card';
 export * from './CardSection';
 export * from './Header';
+export * from './Input.js';
+export * from './Spinner.js';
